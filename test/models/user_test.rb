0fb99ee455540437451ca5c2aa900fd335fed33c
Dan Rodriguez <dcrod@users.noreply.github.com>
2017-03-10 01:51:57 +0000

    Add validation and tests for user name and email presence/length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # create example user
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")  
+  end
+  
+  # assert that user is valid
+  test "user should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = '  '
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+  
+  test "name should be less than 51 characters" do
+    @user.name = "x" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should be less than 256 characters" do
+    @user.email = "x" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
