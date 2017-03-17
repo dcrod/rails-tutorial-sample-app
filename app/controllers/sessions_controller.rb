36d8dedee63e402a6e8f34dd32f18f6386d4c6c8
Dan Rodriguez <dcrod@users.noreply.github.com>
2017-03-17 22:34:45 +0000

    Handle invalid login submissions (with tests)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Welcome back to the sample app!"
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination provided'
+      render 'new'
+    end
   end
   def destroy
     
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "invalid login information" do
+    get login_path
+    assert_template 'sessions/new'
+    assert_select 'form[action="/login"]'
+    post login_path, params: { session: { email: "user@invalid",
+                                          password: "foo" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
