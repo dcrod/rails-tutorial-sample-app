23c5bcdd6934a7792a54a62487ddcfc55c0628cb
Dan Rodriguez <dcrod@users.noreply.github.com>
2017-03-18 20:44:42 +0000

    Add ability to create and track user session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
       flash[:success] = "Welcome back to the sample app!"
       redirect_to user
     else
