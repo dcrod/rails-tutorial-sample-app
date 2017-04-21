154d4c221cd3f6a7e1fea64a79a2cf6656aeb4b4
Dan Rodriguez <dcrod@users.noreply.github.com>
2017-04-21 21:41:11 +0000

    Update home page to show user info and micropost form for authenticated users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,22 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(content: params[:content])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
   
   def destroy
   end
   
+  private
   
+    # Strong params
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
