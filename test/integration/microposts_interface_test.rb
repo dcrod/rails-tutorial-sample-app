8b45d5d3f43ab6255f00205d99c9eea3c152d143
Dan Rodriguez <dcrod@users.noreply.github.com>
2017-04-27 20:39:37 +0000

    Add integration for micropost sidebar count with proper pluralization

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -34,4 +34,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
+  
+  test "micropost sidebar count with proper pluralization" do
+    log_in_as(@user)
+    get root_path
+    assert_select '.user_info>span',
+                  text: "#{@user.microposts.count} microposts"
+    # User with zero microposts (and 1)
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_select '.user_info>span', text: '0 microposts'
+    other_user.microposts.create(content: "A micropost")
+    get root_path
+    assert_select '.user_info>span', text: '1 micropost'
+  end
 end
